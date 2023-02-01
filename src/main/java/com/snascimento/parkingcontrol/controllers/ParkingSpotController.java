b77c2024827fce665a498967157f91a973a3a2e6
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-02-01 09:49:53 -0300

    Ajuste de textos

--- src/main/java/com/snascimento/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/snascimento/parkingcontrol/controllers/ParkingSpotController.java
@@ -39,16 +39,16 @@ public class ParkingSpotController {
   public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
     if (parkingSpotService.existsByLicensePlateCar(parkingSpotDto.getLicensePlateCar())) {
       return ResponseEntity.status(HttpStatus.CONFLICT)
-          .body("Conflict: License Plate Car is already in use!");
+          .body("Conflict: License Plate Car is already in use!!");
     }
     if (parkingSpotService.existsByParkingSpotNumber(parkingSpotDto.getParkingSpotNumber())) {
       return ResponseEntity.status(HttpStatus.CONFLICT)
-          .body("Conflict: Parking Spot is already in use!");
+          .body("Conflict: Parking Spot is already in use!!");
     }
     if (parkingSpotService.existsByApartmentAndBlock(
         parkingSpotDto.getApartment(), parkingSpotDto.getBlock())) {
       return ResponseEntity.status(HttpStatus.CONFLICT)
-          .body("Conflict: Parking Spot already registered for this apartment/block!");
+          .body("Conflict: Parking Spot already registered for this apartment/block!!");
     }
     var parkingSpotModel = new ParkingSpotModel();
     BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
